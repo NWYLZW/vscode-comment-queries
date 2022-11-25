a3d47a104e767c406736b00ec7887e94fdc3bd02
yijie4188 <yijie4188@gmail.com>
2022-11-25 23:29:35 +0800

    refactor: refactor push hint function

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import type * as ts from 'typescript/lib/tsserverlibrary';
 import * as vscode from "vscode";
 
+class ContinueError extends Error {}
+
 export function activate(context: vscode.ExtensionContext) {
   const provider: vscode.InlayHintsProvider = {
     provideInlayHints: async (model, iRange, cancel) => {
@@ -8,8 +10,47 @@ export function activate(context: vscode.ExtensionContext) {
       const text = model.getText(iRange);
       const results: vscode.InlayHint[] = [];
 
-      const m = text.matchAll(/^\s*\/\/\s*(\^|_)(x\d*)?\?/gm);
-      for (const match of m) {
+      async function pushHint(
+        inspectionPos: vscode.Position,
+        endPos: vscode.Position,
+      ) {
+        const { scheme, fsPath, authority, path } = model.uri;
+
+        const hint: any = await vscode.commands.executeCommand(
+          "typescript.tsserverRequest",
+          "quickinfo",
+          {
+            // @ts-ignore
+            __: 'vscode-comment-queries',
+            file: scheme === 'file' ? fsPath : `^/${scheme}/${authority || 'ts-nul-authority'}/${path.replace(/^\//, '')}`,
+            line: inspectionPos.line + 1,
+            offset: inspectionPos.character,
+          } as ts.server.protocol.QuickInfoRequest['arguments']
+        );
+
+        if (!hint || !hint.body) {
+          throw new ContinueError();
+        }
+
+        // Make a one-liner
+        let text = hint.body.displayString
+          .replace(/\/n/g, " ")
+          .replace(/\n */g, "␊")
+          .replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+        if (text.length > 120) {
+          text = text.slice(0, 119) + "...";
+        }
+
+        const inlay: vscode.InlayHint = {
+          kind: 0,
+          position: new vscode.Position(endPos.line, endPos.character + 1),
+          label: text,
+          paddingLeft: true,
+        };
+        results.push(inlay);
+      }
+
+      for (const match of text.matchAll(/^\s*\/\/\s*(\^|_)(x\d*)?\?/gm)) {
         if (match.index === undefined) {
           return;
         }
@@ -33,40 +74,14 @@ export function activate(context: vscode.ExtensionContext) {
           return [];
         }
 
-        const { scheme, fsPath, authority, path } = model.uri;
-
-        const hint: any = await vscode.commands.executeCommand(
-          "typescript.tsserverRequest",
-          "quickinfo",
-          {
-            // @ts-ignore
-            __: 'vscode-comment-queries',
-            file: scheme === 'file' ? fsPath : `^/${scheme}/${authority || 'ts-nul-authority'}/${path.replace(/^\//, '')}`,
-            line: inspectionPos.line + 1,
-            offset: inspectionPos.character,
-          } as ts.server.protocol.QuickInfoRequest['arguments']
-        );
-
-        if (!hint || !hint.body) {
-          continue;
+        try {
+          pushHint(inspectionPos, endPos);
+        } catch (e) {
+          if (e instanceof ContinueError) {
+            continue;
+          }
+          throw e;
         }
-
-        // Make a one-liner
-        let text = hint.body.displayString
-          .replace(/\/n/g, " ")
-          .replace(/\n */g, "␊")
-          .replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
-        if (text.length > 120) {
-          text = text.slice(0, 119) + "...";
-        }
-
-        const inlay: vscode.InlayHint = {
-          kind: 0,
-          position: new vscode.Position(endPos.line, endPos.character + 1),
-          label: text,
-          paddingLeft: true,
-        };
-        results.push(inlay);
       }
       return results;
     },
