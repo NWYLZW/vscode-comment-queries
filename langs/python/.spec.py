6db165f959cfb18a4dbb30734363b322aa3bc129
yijie4188 <yijie4188@gmail.com>
2022-11-26 19:44:44 +0800

    chore: update python spec

--- langs/python/.spec.py
+++ langs/python/.spec.py
@@ -1,12 +1,21 @@
 from typing import Generic, Literal, Optional, TypeVar, TypedDict
 
+D = TypedDict('D', {
+    'e': int,
+    'f': Optional[str],
+})
+
 T = TypedDict('T', {
     'a': int,
     'b': Optional[str],
     'c': Literal["foo", "bar", 'some\ndesc\\n'],
+    'd': D,
 })
 
-t = T(a=1, b=None, c='foo')
+t = T(a=1, b=None, c='foo', d={
+    'e': 1,
+    'f': None,
+})
 
 a = t['a']
 #^? [int]
@@ -16,6 +25,9 @@ b = t['b']
 
 c = t['c']
 #^? [Literal['foo', 'bar', 'some\ndesc\\n']]
+
+d = t['d']
+#^? [D]
 
 KT = TypeVar('KT')
 
