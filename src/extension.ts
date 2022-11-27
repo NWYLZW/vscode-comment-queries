b8e48a05559905ca11bbfaf3813b31ef2c3634ee
yijie4188 <yijie4188@gmail.com>
2022-11-27 16:37:12 +0800

    feature: try support python

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 import * as vscode from "vscode";
 
 import inlayHintsProviderForJSAndTS from "./langs/js&ts";
+import inlayHintsProviderForPY from "./langs/py";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(inlayHintsProviderForJSAndTS());
+  context.subscriptions.push(inlayHintsProviderForPY());
 }
 
 export function deactivate() {}
