de7e5938dc68a89c5a0207880ac0ff989ef2c756
yijie4188 <yijie4188@gmail.com>
2022-11-27 12:41:02 +0800

    refactor: refactor text walker file

--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,15 @@
 import type * as ts from 'typescript/lib/tsserverlibrary';
 import * as vscode from "vscode";
 
-const logger = new Proxy(console, {
+import textCommentWalker, { NoHintError } from './textWalker';
+
+export const logger = new Proxy(console, {
   get() {
     return (...args: any[]) => {
       console.log(`[vscode-comment-queries]`, ...args);
     };
   }
 });
-
-type ProvideInlayHints = vscode.InlayHintsProvider['provideInlayHints'];
-
-type CommentMatcher = [
-  regexp: RegExp,
-  matcher: (
-    endPos: vscode.Position,
-    math: RegExpMatchArray,
-  ) => [insertPos: vscode.Position, hintPos: vscode.Position]
-];
-
-type InlayHint = {
-  matchers: CommentMatcher[];
-  wahtHint: (
-    uri: vscode.Uri,
-    hintPos: vscode.Position
-  ) => Promise<string>;
-};
-
-class NoHintError extends Error {}
-
-function textCommentWalker(
-  inlayHint: InlayHint
-): ProvideInlayHints {
-  return async (model, iRange, cancel) => {
-    const offset = model.offsetAt(iRange.start);
-    const text = model.getText(iRange);
-    const hints: vscode.InlayHint[] = [];
-
-    if (cancel.isCancellationRequested) {
-      return;
-    }
-
-    for await (const [regexp, matcher] of inlayHint.matchers) {
-      for (const match of text.matchAll(regexp)) {
-        if (match.index === undefined) {
-          return;
-        }
-
-        const [insertPos, hintPos] = matcher(
-          model.positionAt(offset + match.index - 1 + match[0].length),
-          match
-        );
-        let hint: string | undefined;
-        try {
-          hint = await inlayHint.wahtHint(model.uri, hintPos);
-        } catch (e) {
-          if (e instanceof NoHintError) {
-            continue;
-          }
-          throw e;
-        }
-        let text = hint
-          .replace(/\/n/g, " ")
-          .replace(/\n */g, "␊")
-          .replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
-        if (text.length > 120) {
-          text = text.slice(0, 119) + "...";
-        }
-
-        hints.push({
-          kind: 0,
-          position: new vscode.Position(insertPos.line, insertPos.character + 1),
-          label: text,
-          paddingLeft: true,
-        });
-      }
-    }
-    return hints;
-  };
-}
 
 function useInlayHintsFor(
   langs: string[],
---
+++ src/textWalker.ts
@@ -0,0 +1,72 @@
+import * as vscode from "vscode";
+
+export type ProvideInlayHints = vscode.InlayHintsProvider['provideInlayHints'];
+
+export type CommentMatcher = [
+  regexp: RegExp,
+  matcher: (
+    endPos: vscode.Position,
+    math: RegExpMatchArray,
+  ) => [insertPos: vscode.Position, hintPos: vscode.Position]
+];
+
+export type InlayHint = {
+  matchers: CommentMatcher[];
+  wahtHint: (
+    uri: vscode.Uri,
+    hintPos: vscode.Position
+  ) => Promise<string>;
+};
+
+export class NoHintError extends Error {}
+
+export default function textCommentWalker(
+  inlayHint: InlayHint
+): ProvideInlayHints {
+  return async (model, iRange, cancel) => {
+    const offset = model.offsetAt(iRange.start);
+    const text = model.getText(iRange);
+    const hints: vscode.InlayHint[] = [];
+
+    if (cancel.isCancellationRequested) {
+      return;
+    }
+
+    for await (const [regexp, matcher] of inlayHint.matchers) {
+      for (const match of text.matchAll(regexp)) {
+        if (match.index === undefined) {
+          return;
+        }
+
+        const [insertPos, hintPos] = matcher(
+          model.positionAt(offset + match.index - 1 + match[0].length),
+          match
+        );
+        let hint: string | undefined;
+        try {
+          hint = await inlayHint.wahtHint(model.uri, hintPos);
+        } catch (e) {
+          if (e instanceof NoHintError) {
+            continue;
+          }
+          throw e;
+        }
+        let text = hint
+          .replace(/\/n/g, " ")
+          .replace(/\n */g, "␊")
+          .replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+        if (text.length > 120) {
+          text = text.slice(0, 119) + "...";
+        }
+
+        hints.push({
+          kind: 0,
+          position: new vscode.Position(insertPos.line, insertPos.character + 1),
+          label: text,
+          paddingLeft: true,
+        });
+      }
+    }
+    return hints;
+  };
+}
