120177bed23f45731088b93fb24bf13a3be8bdf2
yijie4188 <yijie4188@gmail.com>
2022-11-27 19:45:24 +0800

    refactor: sharp queries

--- src/langs/py.ts
+++ src/langs/py.ts
@@ -1,27 +1,17 @@
 import * as vscode from "vscode";
-import logger from '../logger';
 
 import textCommentWalker, { NoHintError } from "../textWalker";
 import useHint from "../useHint";
 import useInlayHintsFor from "../useInlayHintsFor";
+import matchers from "../matchers";
 
 export default () => useInlayHintsFor(
   ["python"],
   textCommentWalker({
     wahtHint: useHint("python"),
     matchers: [
-      [/^\s*#\s*(\^|_)(x\d*)?\?/gm, (endPos, match) => {
-        const [, lineOffset] = match[2]?.match(/x(\d*)/) ?? [, '1'];
-
-        return [endPos, new vscode.Position(
-          endPos.line + ({
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            '^': -1 * Number(lineOffset),
-            '_': 1  * Number(lineOffset),
-          }[match[1] as '^' | '_']),
-          endPos.character
-        )];
-      }],
+      matchers.sharpRelative,
+      matchers.sharpAbsolute,
     ],
   }),
 );
--- src/matchers.ts
+++ src/matchers.ts
@@ -34,5 +34,21 @@ export default narrowCurry<Record<string, CommentMatcher>>()({
   //     endPos.character - 1
   //   );
   //   return [];
-  // }]
+  // }],
+  sharpRelative: [/^\s*#\s*(\^|_)(x\d*)?\?/gm, (endPos, match) => {
+    const [, lineOffset] = match[2]?.match(/x(\d*)/) ?? [, '1'];
+
+    return [endPos, new Position(
+      endPos.line + ({
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        '^': -1 * Number(lineOffset),
+        '_': 1  * Number(lineOffset),
+      }[match[1] as '^' | '_']),
+      endPos.character
+    )];
+  }],
+  sharpAbsolute: [/^\s*#\s*@\[?(\d+)\,\s?(\d+)\]?\?/gm, (endPos, match) => [endPos, new Position(
+    Number(match[1]),
+    Number(match[2])
+  )]],
 });
