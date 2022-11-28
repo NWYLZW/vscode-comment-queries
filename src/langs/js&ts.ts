21798e5a013a99dce3b481398d91e893f016bd7d
yijie4188 <yijie4188@gmail.com>
2022-11-28 11:22:13 +0800

    feat: resolve ts server error

--- src/langs/js&ts.ts
+++ src/langs/js&ts.ts
@@ -5,6 +5,13 @@ import textCommentWalker, { NoHintError } from "../textWalker";
 import useInlayHintsFor from "../useInlayHintsFor";
 import matchers from '../matchers';
 
+function isServerError(error: any): error is {
+  serverId: string;
+  serverMessage: string;
+} {
+  return error.serverId && error.serverMessage;
+}
+
 export default () => useInlayHintsFor(
   ["javascript", "typescript", "typescriptreact", "javascriptreact"],
   textCommentWalker({
@@ -14,23 +21,44 @@ export default () => useInlayHintsFor(
       authority,
       path
     }, hintPos) {
-      const hint = await vscode.commands.executeCommand(
-        "typescript.tsserverRequest",
-        "quickinfo",
-        {
-          // @ts-ignore
-          __: 'vscode-comment-queries',
-          file: scheme === 'file' ? fsPath : `^/${scheme}/${authority || 'ts-nul-authority'}/${path.replace(/^\//, '')}`,
-          line: hintPos.line + 1,
-          offset: hintPos.character,
-        } as ts.server.protocol.QuickInfoRequest['arguments']
-      ) as ts.server.protocol.QuickInfoResponse;
+      let displayString = "";
+      try {
+        const hint = await vscode.commands.executeCommand(
+          "typescript.tsserverRequest",
+          "quickinfo",
+          {
+            // @ts-ignore
+            __: 'vscode-comment-queries',
+            file: scheme === 'file' ? fsPath : `^/${scheme}/${authority || 'ts-nul-authority'}/${path.replace(/^\//, '')}`,
+            line: hintPos.line + 1,
+            offset: hintPos.character,
+          } as ts.server.protocol.QuickInfoRequest['arguments']
+        ) as ts.server.protocol.QuickInfoResponse;
 
-      if (!hint || !hint.body) {
-        throw new NoHintError();
+        if (!hint || !hint.body) {
+          throw new NoHintError();
+        }
+
+        displayString = hint.body.displayString;
+      } catch (err) {
+        if (err instanceof NoHintError) {
+          throw err;
+        }
+        if (err instanceof Error) {
+          if (isServerError(err)) {
+            displayString = err.serverMessage;
+            if (displayString === "No Project.") {
+              displayString += ` (open a folder, or open ${fsPath})`;
+            }
+          } else {
+            displayString = err.message ?? 'Unknown error';
+          }
+          displayString = `Error: ${displayString}`;
+        } else {
+          throw err;
+        }
       }
-
-      return hint.body.displayString;
+      return displayString;
     },
     matchers: [
       matchers.twoSlashRelative,
